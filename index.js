1020142b8c01bfbf7fcc25c29579b2c8a76e73b6
elbert <elbert.lum@gmail.com>
2017-02-06 23:58:35 -0800

    Added function to generate grid

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+var width = 960;
+
 $(document).ready(function() {
-  $('.container').append('<p>This is where the grid will go!</p>');
+  createGrid(16);
 })
+
+function createGrid (dim) {
+  var count = 0;
+  var cellWidth = Math.floor(width / dim);
+  while (count < dim) {
+    $('div.container').append('<tr></tr>');
+    count ++;
+  };
+  $('tr').each(function() {
+    count = 0;
+    while (count < dim) {
+      $(this).append('<td><div class="cell"></div></td>');
+      count ++;
+    }
+  });
+  $('.cell').height(cellWidth).width(cellWidth);
+}
