9e1f6f3eb3045259dc9e80b2d735130d0c83254d
elbert <elbert.lum@gmail.com>
2017-02-07 00:18:39 -0800

    Added color change functionality to cells

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var width = 960;
 
 $(document).ready(function() {
   createGrid(16);
-})
+});
 
 function createGrid (dim) {
   var count = 0;
@@ -19,4 +19,14 @@ function createGrid (dim) {
     }
   });
   $('.cell').height(cellWidth).width(cellWidth);
-}
+};
+
+function clearGrid () {
+  $('div.container').empty();
+};
+
+$(document).on('mouseenter', '.cell', function() {
+  if ($(this).css('background-color') !== 'rgb(0, 0, 0)') {
+    $(this).css('background-color', 'black');
+  }
+});
